b8ec355ab442ad4dd30764d26a9ed9a4d5e4478c
mikejiang <wjiang2@fhcrc.org>
2016-08-18 13:15:14 -0700

    move inst/tests to tests/testthat/

---
+++ tests/testthat.R
@@ -0,0 +1,8 @@
+library(testthat)
+library(ncdfFlow)
+
+test_check("ncdfFlow")
+#devtools::test("~/rglab/workspace/ncdfFlow")
+#test_file("~/rglab/workspace/ncdfFlow/inst/tests/test_ncdfFlowSet_accessor.R")
+#test_file("~/rglab/workspace/ncdfFlow/inst/tests/test_ncdfFlowList.R")
+
---
+++ tests/testthat/helper_data.R
@@ -0,0 +1,3 @@
+data(GvHD)
+morphGate <- norm2Filter("FSC-H", "SSC-H", filterId = "MorphologyGate",scale = 2)
+rectGate <- rectangleGate(filterId="nonDebris","FSC-H"=c(200,Inf))
