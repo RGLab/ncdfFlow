050cdc60372fa916b6b906392665825e7f05d069
Jacob Wagner <jpwagner@fredhutch.org>
2019-07-23 12:50:06 -0700

    Move tests of flowViz ncdfFlowSet plot methods to flowViz

--- tests/testthat/test_ncdfFlowSet_accessor.R
+++ tests/testthat/test_ncdfFlowSet_accessor.R
@@ -1,6 +1,5 @@
 context("ncdfFlowSet accessors")
 library(flowStats)
-library(flowViz)
 morphGate <- norm2Filter("FSC-H", "SSC-H", filterId = "MorphologyGate",scale = 2)
 fs <- GvHD[pData(GvHD)$Patient %in% 6:7][1:4]
 suppressMessages(ncfs <- ncdfFlowSet(fs))
@@ -261,49 +260,6 @@ test_that("colnames<-", {
       expect_equal(nc@origColnames, c(newColNames[2:1],coln[-c(1:2)]))
       is_equal_flowSet(ncfs[sn, coln[2:1]], nc)
       expect_equivalent(unlist(keyword(nc[[1]])[c("$P1N", "$P2N")]), rev(newColNames))
-    })  
-
-test_that("xyplot ncdfFlowSet", {
-      sn <- samples[1:2]
-      nc <- ncfs[sn]
-      ncObj <- xyplot(`SSC-H`~`FSC-H`, nc)
-      
-      expect_is(ncObj[["panel.args.common"]][["frames"]], "ncdfFlowSet")
-      
-      expect_equal(ncObj[["panel.args.common"]][["type"]], "xyplot")
-      
-      fsObj <- xyplot(`SSC-H`~`FSC-H`, fs[sn])
-      
-      expect_is(fsObj[["panel.args.common"]][["frames"]], "environment")
-      
-      expect_equal(sub("type", "plotType", deparse(ncObj[["call"]])), deparse(fsObj[["call"]]))
-      
-      ncObj[["panel.args.common"]][["frames"]] <- NULL
-      fsObj[["panel.args.common"]][["frames"]] <- NULL
-      ncObj[["panel.args.common"]][["type"]] <- NULL
-      ncObj[["call"]] <- NULL
-      fsObj[["call"]] <- NULL
-      
-      expect_equivalent(fsObj, ncObj)
-    })
-
-test_that("densityplot ncdfFlowSet", {
-      sn <- samples[1:2]
-      nc <- ncfs[sn]
-      ncObj <- densityplot(~`SSC-H`, nc)
-      fsObj <- densityplot(~`SSC-H`, fs[sn])
-      
-      expect_is(ncObj[["panel.args.common"]][["frames"]], "ncdfFlowSet")
-      expect_is(fsObj[["panel.args.common"]][["frames"]], "environment")
-
-      expect_equal(deparse(ncObj[["call"]]), deparse(fsObj[["call"]]))
-      
-      ncObj[["panel.args.common"]][["frames"]] <- NULL
-      fsObj[["panel.args.common"]][["frames"]] <- NULL
-      ncObj[["call"]] <- NULL
-      fsObj[["call"]] <- NULL
-      
-      expect_equivalent(fsObj, ncObj)
     })
 
 test_that("split", {
